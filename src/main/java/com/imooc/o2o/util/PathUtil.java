06cf9a8c1eff1e70081a9d1d78c13e89ed192fc0
Jiaxi Zhu <31936629+jiaxizhu@users.noreply.github.com>
2021-02-17 22:32:36 -0800

    shoplist and product detail, add DES encode

--- src/main/java/com/imooc/o2o/util/PathUtil.java
+++ src/main/java/com/imooc/o2o/util/PathUtil.java
@@ -13,7 +13,7 @@ public class PathUtil {
         if(os.toLowerCase().startsWith("win")) {
           basePath = "D:/projectdev/image/";    //根据自己的实际路径进行设置
         }else {
-            basePath = "/Users/jiaxizhu/Documents/image/";//根据自己的实际路径进行设置
+            basePath = "/Users/jiaxizhu/Documents/image";//根据自己的实际路径进行设置
         }
         basePath = basePath.replace("/", seperator);
         return basePath;
@@ -21,7 +21,7 @@ public class PathUtil {
  
     //根据不同的业务需求返回不同的子路径
     public static String getShopImagePath(long shopId) {
-        String imagePath = "upload/item/shop/"+ shopId + "/";
+        String imagePath = "/upload/item/shop/"+ shopId + "/";
         return imagePath.replace("/", seperator);
     }
 }
